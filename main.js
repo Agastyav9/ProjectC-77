9d3a1ea17761ca7405338477b00a7666316196c7
Agastyav9 <73377219+Agastyav9@users.noreply.github.com>
2020-12-16 18:26:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+var Console = "My First Console App";
+console.log(Console);
+
+var First = "Agastya";
+var Last = "Vinod";
+
+var Full = First.concat(Last);
+console.log(Full);
+
+
+var N1 =20;
+
+var N2 =50;
+
+var A1 =N1+N2;
+console.log(A1)
+
+var A2 =N2-N1;
+console.log(A2);
+
+var A3 =N2*N1;
+console.log(A3);
+
+var A4 =N2/N1;
+console.log(A4);
+
+
+
